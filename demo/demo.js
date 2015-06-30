c0c1c38da0abb7d8a69603144f53e29c0085532e
Tuan Le <tuan.le.a@gmail.com>
2015-06-29 14:51:21 -1000

    Revert "Update my forked repo"

--- demo/demo.js
+++ demo/demo.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-    $('#myTable01').fixedHeaderTable({ footer: true, cloneHeadToFoot: true, altClass: 'odd', autoShow: false });
+    $('.myTable01').fixedHeaderTable({ width: '600', height: '250', footer: true, cloneHeadToFoot: true, altClass: 'odd', themeClass: 'fancyTable', autoShow: false });
     
-    $('#myTable01').fixedHeaderTable('show', 1000);
+    $('.myTable01').fixedHeaderTable('show', 1000);
     
-    $('#myTable02').fixedHeaderTable({ footer: true, altClass: 'odd' });
+    $('.myTable02').fixedHeaderTable({ width: '600', height: '250', footer: true, altClass: 'odd', themeClass: 'fancyDarkTable' });
     
-    $('#myTable05').fixedHeaderTable({ altClass: 'odd', footer: true, fixedColumns: 1 });
+    $('.myTable03').fixedHeaderTable({ width: '400', height: '400', altClass: 'odd', footer: true, themeClass: 'fancyDarkTable' });
     
-    $('#myTable03').fixedHeaderTable({ altClass: 'odd', footer: true, fixedColumns: 1 });
-    
-    $('#myTable04').fixedHeaderTable({ altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumns: 3 });
+	$('a.makeTable').bind('click', function() {
+		
+
+		$('.myTable01').fixedHeaderTable('destroy');
+		
+		$('.myTable01 th, .myTable01 td')
+			.css('border', $('#border').val() + 'px solid ' + $('#color').val());
+			
+		$('.myTable01').fixedHeaderTable({ width: $('#width').val(), height: $('#height').val(), footer: true, themeClass: 'fancyTable' });
+	});
 });
