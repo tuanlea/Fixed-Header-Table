244ef04f9274ac7a66ca28454bfb7d41a71400a8
Tuan Le <tuan.le@altres.com>
2015-06-30 07:14:55 -1000

    Updated mousewheel plugin and jQuery library.  demo/test.html has mousewheel plugin added.

--- jquery.fixedheadertable.js
+++ jquery.fixedheadertable.js
@@ -463,25 +463,21 @@
           'width': fixedColumnWidth
         });
 
-
+        
         // bind mousewheel events
-        var maxTop = $fixedColumn.find('.fht-tbody .fht-table').height() - $fixedColumn.find('.fht-tbody').height();
-        $fixedColumn.find('.fht-tbody .fht-table').bind('mousewheel', function(event, delta, deltaX, deltaY) {
-          if (deltaY == 0) {
+        $fixedColumn.find('.fht-tbody .fht-table').on('mousewheel', function(event) {
+          if (event.deltaY == 0) {
             return;
           }
-          var top = parseInt($(this).css('marginTop'), 10) + (deltaY > 0 ? 120 : -120);
-          if (top > 0) {
-            top = 0;
-          }
-          if (top < -maxTop) {
-            top = -maxTop;
-          }
-          $(this).css('marginTop', top);
-          $fixedBody.find('.fht-tbody').scrollTop(-top).scroll();
+          
+          var top = $fixedBody.find('.fht-tbody').scrollTop();
+          var scrollDistance = (-1 * event.deltaY) * event.deltaFactor;
+          var newTop = top + scrollDistance;
+          
+          $fixedBody.find('.fht-tbody').scrollTop(newTop).scroll();
+
           return false;
         });
-
 
         // set width of body table wrapper
         $fixedBody.css({
